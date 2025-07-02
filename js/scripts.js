23f23214b43654b261ada1dfab26c56d56f468be
scarleaf <zzamdoly@gmail.com>
2025-07-03 02:23:40 +0900

    패딩, 달력, 썸네일 수정

--- js/scripts.js
+++ js/scripts.js
@@ -251,18 +251,18 @@ function currentSlide(n) {
 function showSlides(n) {
   let i;
   const slides = document.getElementsByClassName("mySlides");
-  const dots = document.getElementsByClassName("dot");
+  const thumbnails = document.getElementsByClassName("thumbnail");
   if (slides.length === 0) return;
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
+  for (i = 0; i < thumbnails.length; i++) {
+    thumbnails[i].classList.remove("active");
   }
   slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+  thumbnails[slideIndex-1].classList.add("active");
 }
 
 // 자동 슬라이드 (원하면 아래 주석 해제)
