646076e66364c84d3750bc8cf8a4ad58ce7d1e42
scarleaf <zzamdoly@gmail.com>
2025-07-03 01:47:28 +0900

    갤러리 표현 방식 변경

--- js/scripts.js
+++ js/scripts.js
@@ -237,3 +237,31 @@ document.addEventListener('DOMContentLoaded', function() {
   updateCountdown();
   setInterval(updateCountdown, 1000);
 });
+
+// 갤러리
+let slideIndex = 1;
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+function showSlides(n) {
+  let i;
+  const slides = document.getElementsByClassName("mySlides");
+  const dots = document.getElementsByClassName("dot");
+  if (slides.length === 0) return;
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
+// 자동 슬라이드 (원하면 아래 주석 해제)
+// setInterval(() => { plusSlides(1); }, 4000);
